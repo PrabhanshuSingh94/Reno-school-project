c7b84200be62f5db8fd6ef6b225ba16999ccc79c
PrabhanshuSingh <prabhanshusingh94@gmail.com>
2025-09-03 22:35:53 +0530

    Changed the home page 4

--- pages/api/getSchool.js
+++ pages/api/getSchool.js
@@ -8,10 +8,7 @@ export default async function handler(req, res) {
         port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+        database: process.env.DB_NAME
       }
     );
 
